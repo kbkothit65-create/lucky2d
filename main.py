67f50de28c5a7ad9bdb9b2ad4dfc76236505381e
kbkothit65-create <kbkothit65@gmail.com>
2026-08-02 02:09:38 +0630

    main.py

--- main.py
+++ main.py
@@ -1301,5 +1301,4 @@ def main(page: ft.Page):
         page.views.append(show_login_signup_view())
         page.update()
 
-    if name == "main":
-    ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=int(os.environ.get("PORT", 8080)), assets_dir="assets")
+ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=int(os.environ.get("PORT", 8080)), assets_dir="assets")
