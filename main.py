94d452775a586b190639bbe90e2164585cd0e228
kbkothit65-create <kbkothit65@gmail.com>
2026-08-19 10:32:17 +0630

    Update main.py

--- main.py
+++ main.py
@@ -988,7 +988,7 @@ def main(page: ft.Page):
             formatted_date = target_date.strftime("%d.%m.%Y")
 
             try:
-                supabase.table("users").update({"balance": new_bal}).in_("id", current_user["phone"], current_user["name"]).execute()
+                supabase.table("users").update({"balance": new_bal}).filter("id", current_user["phone"], current_user["name"]).execute()
 
                 supabase.table("bets").insert({
                     "user_id": current_user["phone"],
